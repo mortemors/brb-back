c48405903251baf01da0d07ea0e37277cdb67780
Jeferson GB <jefersongb@outlook.com.br>
2020-01-25 20:10:52 -0300

    Configurado para conectar com o mongodb

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,8 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -22,5 +24,6 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
