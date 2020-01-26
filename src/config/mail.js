9b56fa52642f93e88c312a5bb7121c7f4874fc42
Jeferson GB <jefersongb@outlook.com.br>
2020-01-26 00:20:17 -0300

    Configurado o sistema de envio de e-mail em casos de cancelamento.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8654988f6b5760',
+    pass: 'ab1dd5ca7b323e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
