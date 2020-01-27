6aafdaf2656572611568fc21bcefd33904e38453
Jeferson GB <jefersongb@outlook.com.br>
2020-01-26 22:10:36 -0300

    Criado arquivo para variáveis ambiente.

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '8654988f6b5760',
-    pass: 'ab1dd5ca7b323e',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
